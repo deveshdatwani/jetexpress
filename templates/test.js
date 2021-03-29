c4d082c04950ca044d35d07f73b5b798e34c67ef
Deveshd Datwani <datwanidevesh@gmail.com>
2021-03-29 21:49:14 +0530

    new start

--- templates/test.js
+++
@@ -1,8 +0,0 @@
-let btnAdd = document.getElementById('button');
-let table = document.getElementsByTagName('table')[0];
-
-btnAdd.addEventListener('click', () => {
-
-    let template =  " <tr> <td>.</td> <td>.</td> <td>.</td> <td>.</td> <td>.</td> </tr>";
-    table.innerHTML += template;
-});
